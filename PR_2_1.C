974ac1dca80693ba83d175637c735be201955a35
parthitaliya <parthitaliya93@gmail.com>
2023-11-22 18:37:48 +0530

    completed

---
+++ PR_2_1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int n;
+clrscr();
+
+printf("enter any num: ");
+scanf("%d",&n);
+
+(n%2==0)?printf("num is even\n")
+	:printf("num is odd");
+
+	getch();
+	}
+
